5b527a96ff6b5f617cca855c7dd0497175cf33c7
yigitozdamar <yigitozdamar@hotmail.com>
2022-02-14 10:40:12 +0300

    socket.io installed

---
+++ client/src/components/Palette.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      <input type="color" name="color" />
+      <button>Click</button>
+    </div>
+  );
+};
+
+export default Palette;
